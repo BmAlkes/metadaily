a291fda5f55533e937524c156ed9fa496d4a214b
bmalkes <bmalkes@gmail.com>
2023-12-08 12:23:46 +0200

    feat:add style counter page

--- app/watch/page.tsx
+++ app/watch/page.tsx
@@ -1,11 +1,47 @@
+import { PlayCircleIcon } from "lucide-react";
 import React from "react";
 
 const Watch = () => {
   return (
     <main className="container max-w-[1024px] flex flex-col gap-8 px-4 pt-16 pb-8 mr-auto ml-auto ">
-      <h2 className="mt-5 text-3xl font-light text-white font-display text-center">
-        Stop Watch
-      </h2>
+      <div className="flex justify-between items-center">
+        <h2 className="mt-5 text-3xl font-light text-white font-display text-center">
+          Stop Watch
+        </h2>
+        <div>
+          <button>
+            <img src="/item1.png" alt="" />
+          </button>
+          <button>
+            <img src="/item2.png" alt="" />
+          </button>
+        </div>
+      </div>
+      <div className="h-full flex-1 flex items-center justify-center">
+        <form className=" flex flex-col gap-14 items-center">
+          <div className=" w-full flex items-center justify-center text-white gap-2 flex-wrap text-lg font-semibold">
+            <label htmlFor="task"> Will work in</label>
+            <input type="text" id="task" className="" />
+
+            <label htmlFor="minutesAmount">during</label>
+            <input type="number" id="minutesAmount" />
+            <span>minutes.</span>
+          </div>
+          <div className="w-full text-9xl flex gap-4">
+            <span className="bg-gray-500 text-white p-6 rounded-xl">0</span>
+            <span className="bg-gray-500 text-white p-6 rounded-xl">0</span>
+            <span className="bg-gray-500 text-white p-6 rounded-xl">:</span>
+            <span className="bg-gray-500 text-white p-6 rounded-xl">0</span>
+            <span className="bg-gray-500 text-white p-6 rounded-xl">0</span>
+          </div>
+          <button
+            type="submit"
+            className="w-full  bottom-10  text-white bg-green-500 font-display font-regular text-2xl p-2 hover:bg-green-300 rounded-lg max-w-[1024px] flex items-center justify-center gap-3 "
+          >
+            <PlayCircleIcon size={25} /> Start
+          </button>
+        </form>
+      </div>
     </main>
   );
 };
