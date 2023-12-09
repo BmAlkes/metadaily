2c6faa9fe6dc1324654608115b57c719937d712e
bmalkes <bmalkes@gmail.com>
2023-12-09 19:00:20 +0200

    feat:add historic page

---
+++ app/history/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import styles from "./styles.module.css";
+
+const History = () => {
+  const [tasks, setTasks] = useState([
+    {
+      day: new Date().getDate(),
+      task: "Study Js",
+      during: "25:00",
+      start: "",
+      status: "done",
+    },
+    {
+      day: new Date().getDay(),
+      task: "Study React",
+      during: "60:00",
+      start: "",
+      status: "cancel",
+    },
+    {
+      day: new Date().getDay(),
+      task: "Break",
+      during: "15:00",
+      start: "",
+      status: "progress",
+    },
+  ]);
+  return (
+    <main className="container max-w-[1024px] flex flex-col gap-8 px-6 pt-16 pb-8 mr-auto ml-auto ">
+      <div className="flex justify-between items-center">
+        <h2 className="mt-5 text-3xl font-light text-white font-display text-center">
+          Historic
+        </h2>
+        <div>
+          <Link href="/history">
+            <button>
+              <img src="/item1.png" alt="" />
+            </button>
+          </Link>
+          <Link href="/watch">
+            <button>
+              <img src="/item2.png" alt="" />
+            </button>
+          </Link>
+        </div>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th scope="col">Day</th>
+            <th scope="col">Task</th>
+            <th scope="col">During</th>
+            <th scope="col">Start</th>
+            <th scope="col">Status</th>
+          </tr>
+        </thead>
+        <tbody id="body">
+          {tasks.map((item, index) => (
+            <tr className={styles.tr}>
+              <td className={styles.tdLabel} data-label="Day">
+                {item.day}
+              </td>
+              <td className={styles.tdLabel} data-label="Task">
+                {item.task}
+              </td>
+              <td className={styles.tdLabel} data-label="During">
+                {item.during}
+              </td>
+              <td className={styles.tdLabel} data-label="Start">
+                {item.start}
+              </td>
+              {item.status === "done" && (
+                <td
+                  className={`${styles.tdLabel} flex items-center gap-2 justify-center before:bg-green-500`}
+                  data-label="Status"
+                >
+                  {item.status}
+                </td>
+              )}
+              {item.status === "cancel" && (
+                <td
+                  className={`${styles.tdLabel} flex items-center gap-2 justify-center before:bg-red-500`}
+                  data-label="Status"
+                >
+                  {item.status}
+                </td>
+              )}
+              {item.status === "progress" && (
+                <td
+                  className={`${styles.tdLabel} flex items-center gap-2 justify-center before:bg-amber-400`}
+                  data-label="Status"
+                >
+                  {item.status}
+                </td>
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+};
+
+export default History;
--- app/watch/page.tsx
+++ app/watch/page.tsx
@@ -1,7 +1,33 @@
-import { PlayCircleIcon } from "lucide-react";
+"use client";
+import { PlayIcon } from "lucide-react";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 import React from "react";
 
+const newCycleSchema = zod.object({
+  task: zod.string().min(1, "Inform the task"),
+  minutesAmount: zod
+    .number()
+    .min(5, "Cycle must need to be more than 5 minutes")
+    .max(90, "Cycle must be less than 90 minutes"),
+});
+
+interface NewCycleFormData {
+  task: string;
+  minutesAmount: number;
+}
+
 const Watch = () => {
+  const { handleSubmit, register } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleSchema),
+  });
+
+  const handleCreateNewCycle = (data: NewCycleFormData) => {
+    console.log(data.task);
+  };
+
   return (
     <main className="container max-w-[1024px] flex flex-col gap-8 px-6 pt-16 pb-8 mr-auto ml-auto ">
       <div className="flex justify-between items-center">
@@ -9,16 +35,23 @@ const Watch = () => {
           Stop Watch
         </h2>
         <div>
-          <button>
-            <img src="/item1.png" alt="" />
-          </button>
-          <button>
-            <img src="/item2.png" alt="" />
-          </button>
+          <Link href="/history">
+            <button>
+              <img src="/item1.png" alt="" />
+            </button>
+          </Link>
+          <Link href="/watch">
+            <button>
+              <img src="/item2.png" alt="" />
+            </button>
+          </Link>
         </div>
       </div>
       <div className="h-full flex-1 flex items-center justify-center">
-        <form className=" flex flex-col gap-14 items-center">
+        <form
+          className=" flex flex-col gap-14 items-center"
+          onSubmit={handleSubmit(handleCreateNewCycle)}
+        >
           <div className=" w-full flex items-center justify-center text-white gap-2 flex-wrap text-lg font-semibold">
             <label htmlFor="task"> Will work in</label>
             <input
@@ -27,6 +60,7 @@ const Watch = () => {
               className=" bg-transparent border-b-[2px] border-gray-500 h-10 font-bold px-2 fle-1 placeholder:text-center focus:border-green-500 "
               placeholder="Type your project"
               list="task-sugesstions"
+              {...register("task")}
             />
             <datalist id="task-sugesstions">
               <option value="Project 1" />
@@ -37,9 +71,10 @@ const Watch = () => {
             <input
               type="number"
               id="minutesAmount"
-              max={60}
+              max={90}
               step={5}
               min={5}
+              {...register("minutesAmount")}
               className=" bg-transparent w-16 border-b-[2px] border-gray-500 h-10 font-bold px-2 "
             />
             <span>minutes.</span>
@@ -61,9 +96,9 @@ const Watch = () => {
           </div>
           <button
             type="submit"
-            className="w-full  bottom-10  text-white bg-green-500 font-display font-regular text-2xl p-2 hover:bg-green-300 rounded-lg  flex items-center justify-center gap-3 "
+            className="w-full  bottom-10  text-white bg-green-700 font-display font-regular text-2xl p-2 hover:bg-green-500 rounded-lg  flex items-center justify-center gap-3 "
           >
-            <PlayCircleIcon size={25} /> Start
+            <PlayIcon size={25} /> Start
           </button>
         </form>
       </div>
